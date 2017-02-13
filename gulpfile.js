81870b9473449935ab4d527c3771d2f28656dfc2
fbmfbm <fbmaison@gmail.com>
2017-02-13 12:26:08 +0100

    CHORE : Modification des reference jquery dans npm
    Armonisation des référence a JQuery dans npm n( node_modules)
    
    fbm:agence
    )

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ elixir(function(mix) {
 		'services/csv_service.js',
     	])
 	.scripts([
-		'/../../../node_modules/jquery/jquery.js',
+		'/../../../node_modules/jquery/dist/jquery.js',
 		'/../../../node_modules/angular/angular.js',
 		'/../../../node_modules/tether/dist/js/tether.js',
 		'/../../../node_modules/bootstrap/dist/js/bootstrap.js',
