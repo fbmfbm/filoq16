bbeefe7a554b7f9702a72dfd96915316b6a6b55b
Fabien Maison <fabien@fmaison.com>
2019-07-03 12:14:05 +0200

    bug bootstrap modal hide

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,8 @@
                 
             
             <div class="card-body">
-                   
-                    <h4 class="card-title">Gestionnaire</h4>
-                    <h6 class="card-subtitle text-muted">Gestion de vos données en ligne</h6>
+
+                    <h6 class="card-subtitle text-muted">Accès aux données en ligne</h6>
 
                     <br>
                     <div class="card-text fbm-title">
